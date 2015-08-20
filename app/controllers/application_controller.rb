2cc510c740cf037499d149c650c25c060a339a2b
Michael Becco <e4e6d4d5@gmail.com>
2015-08-20 10:11:24 -0700

    Add Pundit Policy For Application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+    def user_not_authorized
+      flash[:alert] = 'The number you have dialed, 555-555-5555 is not available at this time.'
+      redirect_to(request.referrer || root_path)
+    end
 end
