208e6dd404195eb6da481f8c2195bd5c6310a2e1
Michael Becco <e4e6d4d5@gmail.com>
2015-08-20 18:40:45 -0700

    Add Delete Test to Controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,6 +4,7 @@ class PostsControllerTest < ActionController::TestCase
 
   before do
     @user = users(:vic)
+    @post = posts(:one)
   end
 
   def test_index
@@ -15,5 +16,15 @@ class PostsControllerTest < ActionController::TestCase
     sign_in @user
     get :new
     assert_response :success
+  end
+
+  def test_user_cant_delete_another_users_post
+    sign_out @user
+    @shane = users(:shane)
+    sign_in @shane
+    assert_equal 1, Post.all.count
+    refute_difference('Post.count') do
+      delete :destroy, id: @post
+    end
   end
 end
