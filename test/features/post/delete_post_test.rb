0487980e89a534bb8d6937562ad137b17e116a4a
Michael Becco <e4e6d4d5@gmail.com>
2015-08-20 18:54:13 -0700

    Add More Test for Delete

--- test/features/post/delete_post_test.rb
+++ test/features/post/delete_post_test.rb
@@ -4,6 +4,7 @@ feature "Post::DeletePost" do
 
   before do
     @user = users(:vic)
+    @shane = users(:shane)
     @post = posts(:one)
   end
 
@@ -12,7 +13,13 @@ feature "Post::DeletePost" do
     visit post_path(@post)
     page.must_have_content @post.title
     click_link 'Delete Post'
-    save_and_open_page
     page.wont_have_content "Goobye All!"
+    page.wont_have_content @post.title
+  end
+
+  scenario 'No Delete Link for Non Post Owner' do
+    login @shane
+    visit post_path(@post)
+    page.wont_have_content 'Delete'
   end
 end
