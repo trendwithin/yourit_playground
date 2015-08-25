a3e0e597ee796ffb441f2e95948173a8eb64df2b
Michael Becco <e4e6d4d5@gmail.com>
2015-08-25 15:43:21 -0700

    Add Associations to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :taggings
+  has_many :tags, through: :taggings
 
   validates :title, presence: true
   validates :body, presence: true
