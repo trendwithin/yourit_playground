ab521c7b52ae03c803a4151f4ec0ec72ed4fe8d4
Michael Becco <e4e6d4d5@gmail.com>
2015-08-26 13:55:50 -0700

    Add Tag List to Post#Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'tags/:tag', to: 'articles#index', as: :tag
+  get 'tags/:tag', to: 'posts#index', as: :tag
   resources :posts
   devise_for :users
 end
