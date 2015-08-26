3dffa582906eedb8fef9d695f5efa8633141002b
Michael Becco <e4e6d4d5@gmail.com>
2015-08-26 14:27:35 -0700

    Add tag_list to params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,6 +61,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :tag_list)
     end
 end
